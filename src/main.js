e27f5c7cf27170449279336ca57db043a09eedb0
sunliman <m18401744592@163.com>
2019-10-12 11:07:52 +0800

    '首页样式修改'

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import App from './App'
 import router from './router'
 import '@/assets/scss/index.scss'// scss公共变量
 import '@/assets/css/common.css'// css公共变量
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 import VueAwesomeSwiper from 'vue-awesome-swiper'// swiper
 import 'swiper/dist/css/swiper.css'
 Vue.use(VueAwesomeSwiper /* { default global options } */)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
